1de88ef120c151b402e6be3f3d8812377fb4e34e
MartinKostelecky <kosteleckymartin@gmail.com>
2023-11-12 22:50:28 +0100

    Testing Insured person repository using custom repository method with persistance into in-memory H2 database

--- src/main/java/cz/martinkostelecky/insuredpersonsregisterwebapp/repository/InsuredPersonRepository.java
+++ src/main/java/cz/martinkostelecky/insuredpersonsregisterwebapp/repository/InsuredPersonRepository.java
@@ -2,8 +2,15 @@ package cz.martinkostelecky.insuredpersonsregisterwebapp.repository;
 
 import cz.martinkostelecky.insuredpersonsregisterwebapp.entity.InsuredPerson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface InsuredPersonRepository extends JpaRepository<InsuredPerson, Long> {
+    //method annotations containing sql query for execution
+    @Query("" + "SELECT CASE WHEN COUNT(p) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM InsuredPerson p " +
+            "WHERE p.email = ?1")
 
+    Boolean existsByEmail(String email);
 }
